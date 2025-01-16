98bd0f94399d257ece359113da4369d0d982edff
Ryan Henrique <ryanhenriquesilvaa@outlook.com>
2025-01-15 21:35:32 -0300

    Adicionando + config

--- projeto/loja_informatica/views.py
+++ projeto/loja_informatica/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Produto
 
-# Create your views here.
+def lista_produtos(request):
+    produtos = Produto.objects.all() # Busca todos os produtos
+    return render(request, 'loja_informatica/lista_produtos.html' , {'produtos': produtos})
